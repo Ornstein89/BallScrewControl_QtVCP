c15746a3a54bca4afc2d87041509dd6046727695
Anton Nedogarok <nk260an@gmail.com>
2020-11-11 18:46:40 +0300

    Added prerequisites installation

---
+++ prerequisites.sh
@@ -0,0 +1,4 @@
+sudo apt install python-pyqt5 python-dbus.mainloop.pyqt5 python-pyqt5.qtopengl python-pyqt5.qsci python-pyqt5.qtmultimedia qml-module-qtquick-controls gstreamer1.0-plugins-bad libqt5multimedia5-plugins pyqt5-dev-tools python-dev python-setuptools python-pip git
+sudo apt-get install python-pyqt5.qtsvg
+sudo apt-get install qttools5-dev-tools
+sudo apt-get install qttools5.dev
