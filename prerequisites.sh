e685458eef7478994c5d91a3942c02b2ea01545b
Anton Nedogarok <nk260an@gmail.com>
2020-12-29 18:27:11 +0300

    Fixed prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,4 +1,7 @@
-sudo apt install python-pyqt5 python-dbus.mainloop.pyqt5 python-pyqt5.qtopengl python-pyqt5.qsci python-pyqt5.qtmultimedia qml-module-qtquick-controls gstreamer1.0-plugins-bad libqt5multimedia5-plugins pyqt5-dev-tools python-dev python-setuptools python-pip git
-sudo apt-get install python-pyqt5.qtsvg
-sudo apt-get install qttools5-dev-tools
-sudo apt-get install qttools5.dev
+sudo apt install python-pip git python-dev python-setuptools 
+python2 -m pip install openpyxl
+
+# sudo apt install python-pyqt5 python-dbus.mainloop.pyqt5 python-pyqt5.qtopengl python-pyqt5.qsci python-pyqt5.qtmultimedia qml-module-qtquick-controls gstreamer1.0-plugins-bad libqt5multimedia5-plugins pyqt5-dev-tools 
+# sudo apt-get install python-pyqt5.qtsvg
+# sudo apt-get install qttools5-dev-tools
+# sudo apt-get install qttools5.dev
